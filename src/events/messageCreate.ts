029d67f5b54b87393a611e1e0feca2895dcaabfc
Faris CHTATOU <faris.manage@gmail.com>
2022-10-03 01:33:41 +0200

    update handler to reject inline commands

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -3,20 +3,30 @@ import Discord from "discord.js"
 import CONFIG from "../config.json"
 import { DiscordBot } from "../index.d"
 
-// Depreciated
-export default function messageCreate(
+enum MessageErrorResponse {
+  InvalidCommandInput = "Invalid command input !",
+  CommandNotFound = "Command not found !",
+  CommandDepreciated = "Bip bip bip... Please use slash commands instead.",
+}
+
+// @Depreciated
+export default async function messageCreate(
   bot: DiscordBot,
   message: Discord.Message
-): Promise<Discord.Message<boolean>> | undefined {
-  if (message.author.bot) return
-  if (!message.content.startsWith(CONFIG.COMMAND_PREFIX)) return
-  const args = message.content
-    .slice(CONFIG.COMMAND_PREFIX.length)
-    .trim()
-    .split(/ +/g)
-  const commandInput = args.shift()?.toLowerCase()
-  if (!commandInput) return
-  const cmd = bot.commands.get(commandInput)
-  if (!cmd) return message.reply("Command not found !")
-  cmd.run(bot, message, args)
+): Promise<Discord.Message<boolean> | undefined> {
+  if (message.content.startsWith(CONFIG.COMMAND_PREFIX)) {
+    const args = message.content
+      .slice(CONFIG.COMMAND_PREFIX.length)
+      .trim()
+      .split(/ +/g)
+    const commandInput = args.shift()?.toLowerCase()
+    if (!commandInput)
+      return await message.reply(MessageErrorResponse.InvalidCommandInput)
+    const cmd = bot.commands.get(commandInput)
+    if (!cmd) return await message.reply(MessageErrorResponse.CommandNotFound)
+
+    return await message.reply(
+      `${MessageErrorResponse.CommandDepreciated} (/${cmd.name})`
+    )
+  }
 }
