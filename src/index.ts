38ba3ae3e65c63c2dfb121708c135a396caf6b6d
Faris CHTATOU <faris.manage@gmail.com>
2022-10-03 01:32:38 +0200

    add typing to discord collection

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import Discord from "discord.js"
 
 import CONFIG from "./config.json"
-import { DiscordBot } from "./index.d"
+import { DiscordBot, DiscordCommandFile } from "./index.d"
 import { commandsLoader, eventsLoader } from "./loaders"
 
 const intents = new Discord.IntentsBitField(3276799)
 const bot = new Discord.Client({ intents }) as DiscordBot
 
-bot.commands = new Discord.Collection()
+bot.commands = new Discord.Collection<string, DiscordCommandFile>()
 bot.login(CONFIG.DISCORD_TOKEN)
 commandsLoader(bot)
 eventsLoader(bot)
