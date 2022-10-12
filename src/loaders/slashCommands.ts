134922e30d0e710de17aea9764a08955cb98d2fb
Faris CHTATOU <faris.manage@gmail.com>
2022-10-12 18:20:18 +0200

    code refacto in order to make tests easier

--- src/loaders/slashCommands.ts
+++ src/loaders/slashCommands.ts
@@ -1,6 +1,10 @@
 import Discord, { REST, Routes } from "discord.js"
 import chalk from "chalk"
-import { DiscordBot, DiscordCommandOption } from "../types"
+import {
+  DiscordBot,
+  DiscordCommandDocument,
+  DiscordCommandOption,
+} from "../types"
 
 export function addSlashCommandOption(
   slashCommand: Discord.SlashCommandBuilder,
@@ -32,17 +36,25 @@ export function addSlashCommandOption(
   }
 }
 
-export default async function loader(bot: DiscordBot): Promise<void> {
+export function createSlashCommand(
+  command: DiscordCommandDocument
+): Discord.SlashCommandBuilder {
+  return new Discord.SlashCommandBuilder()
+    .setName(command.getName())
+    .setDescription(command.getDescription())
+    .setDMPermission(command.getDmPermission())
+    .setDefaultMemberPermissions(
+      command.getDefaultMemberPermission()?.toString()
+    )
+}
+
+export function createSlashCommands(
+  bot: DiscordBot
+): Discord.SlashCommandBuilder[] {
   const commands: Discord.SlashCommandBuilder[] = []
   bot.commands.forEach((command) => {
     const options = command.getOptions()
-    const slashCommand = new Discord.SlashCommandBuilder()
-      .setName(command.getName())
-      .setDescription(command.getDescription())
-      .setDMPermission(command.getDmPermission())
-      .setDefaultMemberPermissions(
-        command.getDefaultMemberPermission()?.toString()
-      )
+    const slashCommand = createSlashCommand(command)
 
     if (options && options.length > 0) {
       options.forEach((opt) => {
@@ -51,7 +63,11 @@ export default async function loader(bot: DiscordBot): Promise<void> {
     }
     commands.push(slashCommand)
   })
+  return commands
+}
 
+export default async function loader(bot: DiscordBot): Promise<void> {
+  const commands = createSlashCommands(bot)
   const rest = new REST({ version: "10" }).setToken(bot.token?.toString() ?? "")
   try {
     await rest.put(Routes.applicationCommands(bot.user?.id ?? ""), {
