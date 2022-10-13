5069ffbf8df25ce4002b87d98755954015a6d279
Faris CHTATOU <faris.manage@gmail.com>
2022-10-13 02:06:33 +0200

    export enum

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,4 +1,4 @@
-export { default as messageCreate } from "./messageCreate"
+export { default as messageCreate, MessageErrorResponse } from "./messageCreate"
 export { default as interactionCreate } from "./interactionCreate"
 export { default as ready } from "./ready"
 export { default as guildMemberAdd } from "./guildMemberAdd"
