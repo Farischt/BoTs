52600800947faa8130990b93668a073d8f233e8a
Faris CHTATOU <faris.manage@gmail.com>
2022-10-13 02:05:24 +0200

    new command to emit events

---
+++ src/commands/emit.ts
@@ -0,0 +1,63 @@
+import chalk from "chalk"
+import Discord from "discord.js"
+import {
+  DiscordBot,
+  DiscordCommandDocument,
+  DiscordCommandOptions,
+  DiscordCommandData,
+} from "../types"
+
+class EmitCommand extends DiscordCommandDocument {
+  public readonly eventChoices = ["guildMemberAdd", "guildMemberRemove"]
+  public constructor(
+    name: string,
+    description: string,
+    dmPermission: boolean,
+    defaultMemberPermission: Discord.PermissionResolvable | null,
+    options?: DiscordCommandOptions
+  ) {
+    super(name, description, dmPermission, defaultMemberPermission, options)
+  }
+
+  public async run(
+    bot: DiscordBot,
+    message: Discord.ChatInputCommandInteraction
+  ): Promise<void> {
+    const event = message.options.getString("event")
+    if (!event) return
+
+    if (!this.eventChoices.includes(event)) {
+      console.error(chalk.bold.red(`Invalid event ${event}`))
+      await message.reply(`Invalid event : ${event}.`)
+      return
+    }
+    bot.emit(event, message.member)
+    if (!message.member) return
+    await message.reply(
+      `Event ${event} emitted by <@${message.member.user.id}>`
+    )
+  }
+}
+
+export const emitCommandData: DiscordCommandData = {
+  name: "emit",
+  description: "Emit a choosen event",
+  dmPermission: false,
+  defaultMemberPermission: Discord.PermissionFlagsBits.Administrator,
+  options: [
+    {
+      type: "string",
+      name: "event",
+      description: "Event to emit",
+      required: true,
+    },
+  ],
+}
+
+export default new EmitCommand(
+  emitCommandData.name,
+  emitCommandData.description,
+  emitCommandData.dmPermission,
+  emitCommandData.defaultMemberPermission,
+  emitCommandData.options
+)
