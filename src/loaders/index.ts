4f6de817e13f5fd3f494accc84b7a3139759d1f7
Faris CHTATOU <faris.manage@gmail.com>
2022-10-13 02:07:30 +0200

    ode refacto

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,3 +1,8 @@
 export { default as commandsLoader } from "./commands"
 export { default as eventsLoader } from "./events"
-export { default as slashCommandsLoader } from "./slashCommands"
+export {
+  default as slashCommandsLoader,
+  addSlashCommandOption,
+  createSlashCommand,
+  createSlashCommands,
+} from "./slashCommands"
