1cff01fb9fdc530ff451592cc4a8e435f40c4479
Faris CHTATOU <faris.manage@gmail.com>
2022-10-15 01:38:07 +0200

    load before log

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,6 @@
 import Discord from "discord.js"
 import chalk from "chalk"
-import slashCommandsLoader from "../loaders/slashCommands"
+import { slashCommandsLoader } from "../loaders"
 import { DiscordBot } from "../types"
 
 export default async function ready(
@@ -8,6 +8,6 @@ export default async function ready(
   message: Discord.Message
 ): Promise<void> {
   if (!bot.user) return
-  console.log(chalk.blue(`${bot.user.tag} is ready !`))
   await slashCommandsLoader(bot)
+  console.log(chalk.blue(`${bot.user.tag} is ready !`))
 }
