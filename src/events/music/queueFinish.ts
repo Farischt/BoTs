0b8397af3d8e8f79f86a9f1213cfb950aac47771
Faris CHTATOU <faris.manage@gmail.com>
2022-10-19 00:08:54 +0200

    modify message

--- src/events/music/queueFinish.ts
+++ src/events/music/queueFinish.ts
@@ -2,7 +2,9 @@ import { DiscordBot } from "../../types"
 
 export default async function queueFinish(bot: DiscordBot): Promise<void> {
   bot.music.on("queueFinish", async (queue) => {
-    await queue.channel.send("Uh oh, the queue has ended :/")
+    await queue.channel.send(
+      "`Music player stopped`, queue is empty! Run `play` command to start playing music again."
+    )
     queue.player.disconnect()
     queue.player.node.destroyPlayer(queue.player.guildId)
   })
